c8a00dbd135c9db8631bf679cf4d91aa0695aea2
yarmdy <jxy2000121@sina.com>
2025-02-20 16:00:35 +0800

    测试是不是一个

--- MinimumApiTest/HomeClass.cs
+++ MinimumApiTest/HomeClass.cs
@@ -1,8 +1,14 @@
 ﻿public class HomeClass : ClassRequestHandler
 {
+    AsyncLocal<DateTime> _date;
+    public HomeClass() 
+    {
+        _date=new AsyncLocal<DateTime>();
+        _date.Value = DateTime.Now;
+    }
     public async Task<IResult> Index()
     {
-        return await Task.FromResult(Results.Content("首页"));
+        return await Task.FromResult(Results.Content($"首页{_date.Value}"));
     }
 
     public IResult About(string content)
