6b5e9ca700285d93e862d5a79f309bb3e07c969e
yarmdy <jxy2000121@sina.com>
2025-03-17 10:49:16 +0800

    修改特性筛选

--- MinimumApiTest/ClassRequestHandler.cs
+++ MinimumApiTest/ClassRequestHandler.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Routing.Patterns;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Patterns;
 using System.Collections.Concurrent;
 using System.Reflection;
 
 public abstract class ClassRequestHandler : IClassRequestHandler
 {
     private ConcurrentDictionary<string,MethodInfo?> methods = new ConcurrentDictionary<string,MethodInfo?>();
+    private static readonly HashSet<string> noMapMethodNames = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase) {
+        nameof(IClassDelegateRequestHandler.MapDelegate),
+        nameof(IClassRequestHandler.MapMethodInfo),
+    };
     public virtual MethodInfo? MapMethodInfo(HttpContext context, RoutePattern pattern, RouteData route)
     {
         var patternName = pattern.Parameters.Where(a => a.IsParameter && !a.IsOptional && !a.IsCatchAll).LastOrDefault()?.Name;
@@ -17,6 +22,22 @@ public abstract class ClassRequestHandler : IClassRequestHandler
         {
             return null;
         }
-        return methods.GetOrAdd(action + "", action => GetType().GetMethod(action, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase));
+        if (noMapMethodNames.Contains(action + ""))
+        {
+            return null;
+        }
+        return methods.GetOrAdd(action + "", action => {
+            var method =  GetType().GetMethod(action, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+            if (method == null)
+            {
+                return null;
+            }
+            var attr = method.GetCustomAttribute<NonActionAttribute>(true);
+            if (attr != null)
+            {
+                return null;
+            }
+            return method;
+        });
    }
 }
